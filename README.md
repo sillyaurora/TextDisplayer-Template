15928b7746cb2924dd9b8476f05b2df6585ee64b
sillyaurora <pazonneilcedrickvoid@gmail.com>
2024-10-05 13:18:39 +0800

    new

---
+++ README.md
@@ -0,0 +1,8 @@
+Hello! This is an template for the text displaying window box program with is created by me. If you wish to edit or create your own. Open installer or Source Code and the solution file is placed there.
+# Adding a new word
+1. Add an remote button.
+2. add on code: `else if (name.IfClicked == True) 
+{
+    MessageBox.show("name");
+}` replace name with input
+3. Launch and done. Report problem if has.
---
+++ Source Code/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextDisplayer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Test.IsChecked == true)
+            {
+                MessageBox.Show("Test");
+            }
+            else if (Hello.IsChecked == true)
+            {
+                MessageBox.Show("Hello");
+            }
+            else if (hi.IsChecked == true)
+            {
+                MessageBox.Show("Hi");
+            }
+            else if (option1.IsChecked == true)
+            {
+                MessageBox.Show("1");
+            }
+            else if (Lol.IsChecked == true)
+            {
+                MessageBox.Show("Lol");
+            }
+            else if (Cool.IsChecked == true)
+            {
+                MessageBox.Show("Cool");
+            }
+            else if (Idk.IsChecked == true)
+            {
+                MessageBox.Show("Idk");
+            }
+        }
+
+        // Optional: You can handle the RadioButton Checked event if needed
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            // Handle event if needed
+        }
+
+        private void Lol_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cool_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Idk_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
